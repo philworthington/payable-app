728fe9f4f23bb48569075c4d6beb61c0e0f2716e
Phil Worthington <pworthington27@gmail.com>
2014-01-13 18:44:57 -0800

    Charges controller, static pages and routes set up

---
+++ README.md
@@ -0,0 +1,18 @@
+# Payable
+By: Kangie Chan, [Phil Worthington](http://github.com/philworthington), and [Ali El-Ashram](http://aliel.herokuapp.com)
+
+## Description
+Application that assists users in sending invoices and making payments quickly and easily utilizing the stripe api.
+
+## Information
+
+![Screenshot 1](app/assets/images/)
+
+![Screenshot 2](app/assets/images/)
+
+## Authors
+* Kangie Chan
+* Phil Worthington (github.com/philworthington)
+* Ali El-Ashram (github.com/alielashram)
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Payme::Application.config.secret_key_base = '834ecd2b9546a4cf9a80f841dedbc33bf077e7736ebbac03a908301470b55e96d1e06b2ce3f4549fc7cd187632a91675692e6d6ecf4b1a75702cfd0122bb3f45'
